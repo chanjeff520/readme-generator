1d1da75ffe84d7920ac841e400868a517d8ca4bf
chanjeff520 <chanjeff520@gmail.com>
2022-10-20 12:42:59 -0400

    add render badge

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [""];
+const questions = [
+    {
+        type: 'input',
+        message: "What is the name of your project?  ",
+        name: 'projectName',
+        //validate: (answer) => answer.length < 1 ? console.log("Your project name needs more than one character.") : return true;
+    },
+    {
+        type: 'input',
+        message: 'Write a short description of your project.  ',
+        name: 'descripiton',
+    },
+    {
+        type: 'input',
+        message: "What kind of license should your project have?  ",
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: "What command should be run to install dependencies?  ",
+        name: 'dependencies'
+    },
+    {
+        type: 'input',
+        message: "What command should be run to run tests?  ",
+        name: "testCommands"
+    },
+    {
+        type: 'input',
+        message: "What is your GitHub username?  ",
+        name: "username"
+    },
+    {
+        type: 'input',
+        message: "What is your email address?  ",
+        name: "userEmail"
+    }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
